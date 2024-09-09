42dfc10d4f72502ddf95b9fb921e4425e4841e31
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-09 16:09:39 +0700

    feat: add app color

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xff4A9EF7);
+  static const Color black = Color(0xff494A4B);
+  static const Color white = Color(0xffffffff);
+}
