2063a51e7f94b3f7622926c97c0f0253f889fda0
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-09 16:08:49 +0700

    feat: add assets svg and fonts

---
+++ lib/core/constants/svg_assets.dart
@@ -0,0 +1,6 @@
+class SvgAssets {
+  static const String _path = 'assets/svg';
+  static const String circleHome = '$_path/circle_home.svg';
+  static const String sun = '$_path/sun.svg';
+  static const String cloud = '$_path/cloud.svg';
+}
