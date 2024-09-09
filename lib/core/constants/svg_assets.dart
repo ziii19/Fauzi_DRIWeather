fb085f7fc84c5a57a9c7a217fc3bb7362fa8aa57
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-09 17:08:42 +0700

    feat: add assets svg

--- lib/core/constants/svg_assets.dart
+++ lib/core/constants/svg_assets.dart
@@ -3,4 +3,10 @@ class SvgAssets {
   static const String circleHome = '$_path/circle_home.svg';
   static const String sun = '$_path/sun.svg';
   static const String cloud = '$_path/cloud.svg';
+  static const String map = '$_path/map.svg';
+  static const String cloudy = '$_path/cloudy.svg';
+  static const String rain = '$_path/rain.svg';
+  static const String sunCloudy = '$_path/sun_cloudy.svg';
+  static const String sunny = '$_path/sunny.svg';
+  static const String thunder = '$_path/thunder.svg';
 }
