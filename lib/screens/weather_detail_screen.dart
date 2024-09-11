c31bc942d4b5495f689c02454c87ec6540265106
ziiii08 <fauzifajaralfalah56@gmail.com>
2024-09-11 07:04:49 +0700

    feat: update weather detail screen

--- lib/screens/weather_detail_screen.dart
+++ lib/screens/weather_detail_screen.dart
@@ -1,9 +1,13 @@
+import 'package:fauzi_driweather/core/constants/weather_switch.dart';
 import 'package:fauzi_driweather/core/theme/app_color.dart';
+import 'package:fauzi_driweather/core/utils/date_format.dart';
 import 'package:fauzi_driweather/screens/widgets/back_button.dart';
 import 'package:fauzi_driweather/screens/widgets/bg_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_icons/weather_icons.dart';
 
+import 'bloc/weather_bloc.dart';
 import 'widgets/weather_hourly.dart';
 import 'widgets/weather_next_day.dart';
 
@@ -13,113 +17,123 @@ class WeatherScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BgScaffold(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SafeArea(
-              child: WidgetBack(),
-            ),
-            const SizedBox(height: 50),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Today',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w900,
+      body: BlocBuilder<WeatherBloc, WeatherState>(
+        builder: (context, state) {
+          if (state is WeatherSuccess) {
+            return BgScaffold(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SafeArea(
+                    child: WidgetBack(),
                   ),
-                ),
-                Text(
-                  'Sep, 12',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
+                  const SizedBox(height: 50),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Today',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Text(
+                        'Sep, 12',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 32),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                WeatherHourlyItem(
-                    time: '15.00', temp: '29°C', icon: Icons.wb_cloudy),
-                WeatherHourlyItem(
-                    time: '16.00', temp: '26°C', icon: Icons.wb_cloudy),
-                WeatherHourlyItem(
-                    time: '17.00', temp: '24°C', icon: Icons.cloud),
-                WeatherHourlyItem(
-                    time: '18.00', temp: '23°C', icon: Icons.wb_cloudy),
-                WeatherHourlyItem(
-                    time: '19.00', temp: '22°C', icon: Icons.wb_cloudy),
-              ],
-            ),
-            const SizedBox(height: 50),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Next Forecast',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w900,
+                  const SizedBox(height: 32),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: state.weatherHourly
+                          .map((e) => WeatherHourlyItem(
+                              time: formatTimeHHmm(e.time),
+                              temp: '${e.temperature.toInt()}°',
+                              icon: getWeatherIconAndDescription(
+                                  e.weatherCode)['icon']))
+                          .toList(),
+                    ),
                   ),
-                ),
-                Icon(
-                  Icons.calendar_today,
-                  color: AppColor.white,
-                  size: 20,
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView(
-                children: const [
-                  WeatherNextDayItem(
-                      date: 'Sep, 13',
-                      temp: '21°',
-                      icon: WeatherIcons.day_rain),
-                  WeatherNextDayItem(
-                      date: 'Sep, 14',
-                      temp: '22°',
-                      icon: WeatherIcons.day_cloudy_windy),
-                  WeatherNextDayItem(
-                      date: 'Sep, 15',
-                      temp: '34°',
-                      icon: WeatherIcons.day_cloudy_windy),
-                  WeatherNextDayItem(
-                      date: 'Sep, 16',
-                      temp: '27°',
-                      icon: WeatherIcons.day_cloudy_windy),
-                  WeatherNextDayItem(
-                      date: 'Sep, 17',
-                      temp: '32°',
-                      icon: WeatherIcons.day_cloudy_windy),
+                  const SizedBox(height: 50),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Next Forecast',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Icon(
+                        Icons.calendar_today,
+                        color: AppColor.white,
+                        size: 20,
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: ListView(
+                      children: state.weatherDaily
+                          .map(
+                            (e) => WeatherNextDayItem(
+                              date: formatTimeMMMdd(e.time),
+                              temp: '${e.temperature.toInt()}°',
+                              icon: getWeatherIconAndDescription(
+                                  e.weatherCode)['icon'],
+                            ),
+                          )
+                          .toList(),
+                      // const [
+                      //   WeatherNextDayItem(
+                      //       date: 'Sep, 14',
+                      //       temp: '22°',
+                      //       icon: WeatherIcons.day_cloudy_windy),
+                      //   WeatherNextDayItem(
+                      //       date: 'Sep, 15',
+                      //       temp: '34°',
+                      //       icon: WeatherIcons.day_cloudy_windy),
+                      //   WeatherNextDayItem(
+                      //       date: 'Sep, 16',
+                      //       temp: '27°',
+                      //       icon: WeatherIcons.day_cloudy_windy),
+                      //   WeatherNextDayItem(
+                      //       date: 'Sep, 17',
+                      //       temp: '32°',
+                      //       icon: WeatherIcons.day_cloudy_windy),
+                      // ],
+                    ),
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.wb_sunny, color: Colors.white),
+                      SizedBox(width: 10),
+                      Text(
+                        'DRI Weather',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.wb_sunny, color: Colors.white),
-                SizedBox(width: 10),
-                Text(
-                  'DRI Weather',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
